6629ec52a090438a10f5a77b4f148c7ddbe0b3d5
eriknathan <eriknathan.contato@gmail.com>
2024-03-11 13:57:38 -0300

    feat: script de chamar pipeline

--- vars/callPipeline.groovy
+++ vars/callPipeline.groovy
@@ -15,22 +15,22 @@ def call (Map pipelineParams) {
 			}
 		}
 		post {
-			success {
-					script {
-					echo " --------------------------------------------------------------------------------------- "
-					echo " AUTOMAÇÃO DO ROBOT "
-					echo " --------------------------------------------------------------------------------------- "
+			// success {
+			// 		script {
+			// 		echo " --------------------------------------------------------------------------------------- "
+			// 		echo " AUTOMAÇÃO DO ROBOT "
+			// 		echo " --------------------------------------------------------------------------------------- "
 
-					def scriptpython = libraryResource 'com/scripts/call_pipeline.py'
-					writeFile file: './call_pipeline.py', text: scriptpython
+			// 		def scriptpython = libraryResource 'com/scripts/call_pipeline.py'
+			// 		writeFile file: './call_pipeline.py', text: scriptpython
 
-                    sh 'pip install jenkinsapi'
-					// sh 'python3 ./call_pipeline.py Teste/pipeline-chamada'
-					// echo "Limpando arquivos temporários..."
-					// sh "sudo rm call_pipeline.py"
-				}
-			}
-            always {
+            //         sh 'pip install jenkinsapi'
+			// 		sh 'python3 ./call_pipeline.py Teste/pipeline-chamada'
+			// 		echo "Limpando arquivos temporários..."
+			// 		sh "sudo rm call_pipeline.py"
+			// 	}
+			// }
+            success {
                 mail to: 'eriknathan.contato@gmail.com',
                 subject: "Pipeline: ${currentBuild.fullDisplayName}",
                 body: "Something is wrong with ${env.BUILD_URL}"
