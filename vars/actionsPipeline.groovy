32537b3ca30d392d62f24a75384f9adb5f5dab5c
eriknathan <eriknathan.contato@gmail.com>
2024-05-07 09:17:54 -0300

    testando copiar pipeline do actions

--- vars/actionsPipeline.groovy
+++ vars/actionsPipeline.groovy
@@ -25,6 +25,13 @@ def call (Map pipelineParams) {
 						echo " COPIANDO O ACTIONS PARA DIRETORIO ATUAL "
 						echo " --------------------------------------------------------------------------------------- "
 						
+						sh "mkdir teste1"
+						sh "mkdir teste2"
+						sh "mkdir teste3"
+						sh "mkdir teste4"
+						sh "touch erik.txt"
+						sh "touch erik2.txt"
+
 						def scriptbash = libraryResource 'com/actions/projeto01/ci.yaml'
 						writeFile file: './ci.yaml', text: scriptbash
 						sh "mkdir -p .github/workflows/ && cp ./ci.yaml .github/workflows/"
